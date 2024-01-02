77ca0e6bfd3b0a79827152f04d649c5b9e63ffeb
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2024-01-02 22:55:10 +0100

    Added more test cases for inheritance selection TODO: * Fix problem for inheritance entities in `BlazeTupleInstantiator` * Embedded creator for field present on one inherited entity is called on all subclasses even though they do not have it!

--- bee.persistent.test/src/test/kotlin/com/beeproduced/bee/persistent/test/BeePersistentTestB.kt
+++ bee.persistent.test/src/test/kotlin/com/beeproduced/bee/persistent/test/BeePersistentTestB.kt
@@ -16,6 +16,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.support.TransactionTemplate
 import java.util.*
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 /**
@@ -46,6 +48,12 @@ class BeePersistentTestB(
             val selection = BeeSelection.create {  }
             val composers = composerRepository.select(selection)
             assertTrue { composers.count() == 2 }
+            val c1 = composers[0] as? AiComposer
+            assertNotNull(c1)
+            assertNull(c1.aiData)
+            val c2 = composers[1] as? HumanComposer
+            assertNotNull(c2)
+            assertNull(c2.humanData)
         }
     }
 
@@ -59,8 +67,107 @@ class BeePersistentTestB(
             }
             val composers = composerRepository.select(selection)
             assertTrue { composers.count() == 2 }
+            val c1 = composers[0] as? AiComposer
+            assertNotNull(c1)
+            assertNotNull(c1.aiData)
+            val c2 = composers[1] as? HumanComposer
+            assertNotNull(c2)
+            assertNotNull(c2.humanData)
         }
+    }
 
+    @Test
+    fun `container - empty selection`() {
+        addComposers()
+        transaction.executeWithoutResult {
+            val selection = BeeSelection.create {  }
+            val container = containerRepository.select(selection).firstOrNull()
+            assertNotNull(container)
+            assertNull(container.c1)
+            assertNull(container.c2)
+        }
+    }
+
+    @Test
+    fun `container - full selection`() {
+        addComposers()
+        transaction.executeWithoutResult {
+            val selection = BeeSelection.create {
+                field("c1") {
+                    field("aiData") { field("id") }
+                    field("humanData") { field("id") }
+                }
+                field("c2") {
+                    field("aiData") { field("id") }
+                    field("humanData") { field("id") }
+                }
+            }
+            val container = containerRepository.select(selection).firstOrNull()
+            assertNotNull(container)
+            val c1 = container.c1 as? AiComposer
+            assertNotNull(c1)
+            assertNotNull(c1.aiData)
+            val c2 = container.c2 as? HumanComposer
+            assertNotNull(c2)
+            assertNotNull(c2.humanData)
+        }
+    }
+
+    @Test
+    fun `container - partial selection 1`() {
+        addComposers()
+        transaction.executeWithoutResult {
+            val selection = BeeSelection.create {
+                field("c1") {
+                    field("aiData") { field("id") }
+                    field("humanData") { field("id") }
+                }
+            }
+            val container = containerRepository.select(selection).firstOrNull()
+            assertNotNull(container)
+            val c1 = container.c1 as? AiComposer
+            assertNotNull(c1)
+            assertNotNull(c1.aiData)
+            assertNull(container.c2)
+        }
+    }
+
+    @Test
+    fun `container - partial selection 2`() {
+        addComposers()
+        transaction.executeWithoutResult {
+            val selection = BeeSelection.create {
+                field("c1") {
+                    field("id")
+                }
+            }
+            val container = containerRepository.select(selection).firstOrNull()
+            assertNotNull(container)
+            val c1 = container.c1 as? AiComposer
+            assertNotNull(c1)
+            assertNull(c1.aiData)
+            assertNull(container.c2)
+        }
+    }
+
+
+    @Test
+    fun `container - partial selection 3`() {
+        addComposers()
+        transaction.executeWithoutResult {
+            val selection = BeeSelection.create {
+                field("c2") {
+                    field("aiData") { field("id") }
+                    field("humanData") { field("id") }
+                }
+            }
+            val container = containerRepository.select(selection).firstOrNull()
+            assertNotNull(container)
+            assertNull(container.c1)
+            val c2 = container.c2 as? HumanComposer
+            assertNotNull(c2)
+            assertNotNull(c2.humanData)
+        }
     }
 
     @BeforeAll
@@ -79,13 +186,14 @@ class BeePersistentTestB(
             val aiComposer = em.beePersist(AiComposer(UUID.randomUUID(), "AI", "GPT", AiParams("1", "2"), aiData.id))
             val humanData = em.beePersist(HumanData(UUID.randomUUID(), "Bar"))
             val humanComposer = em.beePersist(HumanComposer(UUID.randomUUID(), "Mario", "Mario", humanData.id))
+            val container = em.beePersist(ComposerContainer(UUID.randomUUID(), aiComposer.id, null, humanComposer.id, null))
         }
     }
 
     fun clear() {
         transaction.executeWithoutResult {
-            composerRepository.cbf.delete(em, Composer::class.java).executeUpdate()
             containerRepository.cbf.delete(em, ComposerContainer::class.java).executeUpdate()
+            composerRepository.cbf.delete(em, Composer::class.java).executeUpdate()
         }
     }
 }
