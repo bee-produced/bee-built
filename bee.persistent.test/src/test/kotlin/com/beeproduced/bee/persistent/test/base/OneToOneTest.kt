623e805579152830a19d8f9d977537c456ce584d
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2024-12-23 10:21:08 +0100

    Added ktfmt to `bee.persistent.test` & reformatted library

--- bee.persistent.test/src/test/kotlin/com/beeproduced/bee/persistent/test/base/OneToOneTest.kt
+++ bee.persistent.test/src/test/kotlin/com/beeproduced/bee/persistent/test/base/OneToOneTest.kt
@@ -8,6 +8,10 @@ import com.beeproduced.datasource.test.onetoone.BranchRepository
 import com.beeproduced.datasource.test.onetoone.Root
 import com.beeproduced.datasource.test.onetoone.RootRepository
 import jakarta.persistence.EntityManager
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
@@ -19,14 +23,8 @@ import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.support.TransactionTemplate
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
-import kotlin.test.assertNull
 
 /**
- *
- *
  * @author Kacper Urbaniec
  * @version 2024-01-14
  */
@@ -35,220 +33,199 @@ import kotlin.test.assertNull
 @TestPropertySource("classpath:application.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class OneToOneTest(
-    @Qualifier("testEM")
-    val em: EntityManager,
-    @Qualifier("testTM")
-    transactionManager: PlatformTransactionManager,
-    @Autowired
-    val rootRepo: RootRepository,
-    @Autowired
-    val branchRepo: BranchRepository
+  @Qualifier("testEM") val em: EntityManager,
+  @Qualifier("testTM") transactionManager: PlatformTransactionManager,
+  @Autowired val rootRepo: RootRepository,
+  @Autowired val branchRepo: BranchRepository,
 ) {
-    private val transaction = TransactionTemplate(transactionManager)
+  private val transaction = TransactionTemplate(transactionManager)
 
-    @BeforeAll
-    fun beforeAll() = clear()
+  @BeforeAll fun beforeAll() = clear()
 
-    @AfterEach
-    fun afterEach() = clear()
+  @AfterEach fun afterEach() = clear()
 
-    fun clear() = transaction.executeWithoutResult {
-        rootRepo.cbf.delete(em, Root::class.java).executeUpdate()
-        branchRepo.cbf.delete(em, Branch::class.java).executeUpdate()
+  fun clear() =
+    transaction.executeWithoutResult {
+      rootRepo.cbf.delete(em, Root::class.java).executeUpdate()
+      branchRepo.cbf.delete(em, Branch::class.java).executeUpdate()
     }
 
-    @Test
-    fun `select with relations not loaded`() {
-        var rootId: Long = -1
-        var branchAId: Long
-        var branchBId: Long
-        var branchAAId: Long
-        var branchABId: Long
-        transaction.executeWithoutResult {
-            val root = rootRepo.persist(Root())
-            rootId = root.id
-            val branchA = branchRepo.persist(Branch())
-            branchAId = branchA.id
-            branchBId = branchRepo.persist(Branch()).id
-            branchAAId = branchRepo.persist(Branch()).id
-            branchABId = branchRepo.persist(Branch()).id
+  @Test
+  fun `select with relations not loaded`() {
+    var rootId: Long = -1
+    var branchAId: Long
+    var branchBId: Long
+    var branchAAId: Long
+    var branchABId: Long
+    transaction.executeWithoutResult {
+      val root = rootRepo.persist(Root())
+      rootId = root.id
+      val branchA = branchRepo.persist(Branch())
+      branchAId = branchA.id
+      branchBId = branchRepo.persist(Branch()).id
+      branchAAId = branchRepo.persist(Branch()).id
+      branchABId = branchRepo.persist(Branch()).id
 
-            rootRepo.update(
-                root.copy(branchAKey = branchAId, branchBKey = branchBId)
-            )
-            branchRepo.update(
-                branchA.copy(branchAKey = branchAAId, branchBKey = branchABId)
-            )
-        }
-
-        transaction.executeWithoutResult {
-            val selection = BeeSelection.empty()
-            // TODO: Replace with selectById
-            val root = rootRepo.select(selection) {
-                where(RootDSL.id.eq(rootId))
-            }.firstOrNull()
-
-            assertNotNull(root)
-            assertNull(root.branchA)
-            assertNull(root.branchB)
-        }
+      rootRepo.update(root.copy(branchAKey = branchAId, branchBKey = branchBId))
+      branchRepo.update(branchA.copy(branchAKey = branchAAId, branchBKey = branchABId))
     }
 
-    @Test
-    fun `select with relations loaded`() {
-        var rootId: Long = -1
-        var branchAId: Long = -1
-        var branchBId: Long = -1
-        var branchAAId: Long = -1
-        var branchABId: Long = -1
-        transaction.executeWithoutResult {
-            val root = rootRepo.persist(Root())
-            rootId = root.id
-            val branchA = branchRepo.persist(Branch())
-            branchAId = branchA.id
-            branchBId = branchRepo.persist(Branch()).id
-            branchAAId = branchRepo.persist(Branch()).id
-            branchABId = branchRepo.persist(Branch()).id
+    transaction.executeWithoutResult {
+      val selection = BeeSelection.empty()
+      // TODO: Replace with selectById
+      val root = rootRepo.select(selection) { where(RootDSL.id.eq(rootId)) }.firstOrNull()
 
-            rootRepo.update(
-                root.copy(branchAKey = branchAId, branchBKey = branchBId)
-            )
-            branchRepo.update(
-                branchA.copy(branchAKey = branchAAId, branchBKey = branchABId)
-            )
-        }
+      assertNotNull(root)
+      assertNull(root.branchA)
+      assertNull(root.branchB)
+    }
+  }
 
-        transaction.executeWithoutResult {
-            val selection = RootDSL.select {
-                this.branchA {
-                    this.branchA {
-                        this.branchA { }
-                        this.branchB {  }
-                    }
-                    this.branchB {
-                        this.branchA {  }
-                        this.branchB {  }
-                    }
-                }
-                this.branchB {
-                    this.branchA {
-                        this.branchA { }
-                        this.branchB {  }
-                    }
-                    this.branchB {
-                        this.branchA {  }
-                        this.branchB {  }
-                    }
-                }
+  @Test
+  fun `select with relations loaded`() {
+    var rootId: Long = -1
+    var branchAId: Long = -1
+    var branchBId: Long = -1
+    var branchAAId: Long = -1
+    var branchABId: Long = -1
+    transaction.executeWithoutResult {
+      val root = rootRepo.persist(Root())
+      rootId = root.id
+      val branchA = branchRepo.persist(Branch())
+      branchAId = branchA.id
+      branchBId = branchRepo.persist(Branch()).id
+      branchAAId = branchRepo.persist(Branch()).id
+      branchABId = branchRepo.persist(Branch()).id
+
+      rootRepo.update(root.copy(branchAKey = branchAId, branchBKey = branchBId))
+      branchRepo.update(branchA.copy(branchAKey = branchAAId, branchBKey = branchABId))
+    }
+
+    transaction.executeWithoutResult {
+      val selection =
+        RootDSL.select {
+          this.branchA {
+            this.branchA {
+              this.branchA {}
+              this.branchB {}
             }
-
-            // TODO: Replace with selectById
-            val root = rootRepo.select(selection) {
-                where(RootDSL.id.eq(rootId))
-            }.firstOrNull()
-
-            assertNotNull(root)
-            assertEquals(rootId, root.id)
-            val branchA = root.branchA
-            assertNotNull(branchA)
-            assertEquals(branchAId, branchA.id)
-            val branchAA = branchA.branchA
-            assertNotNull(branchAA)
-            assertEquals(branchAAId, branchAA.id)
-            assertNull(branchAA.branchA)
-            assertNull(branchAA.branchB)
-            val branchAB = branchA.branchB
-            assertNotNull(branchAB)
-            assertEquals(branchABId, branchAB.id)
-            assertNull(branchAB.branchA)
-            assertNull(branchAB.branchB)
-
-            val branchB = root.branchB
-            assertNotNull(branchB)
-            assertEquals(branchBId, branchB.id)
-            assertNull(branchB.branchA)
-            assertNull(branchB.branchB)
-        }
-    }
-
-    @Test
-    fun `select recursive selection`() {
-        var rootId: Long = -1
-        var branchAId: Long = -1
-        var branchBId: Long = -1
-        var branchABId: Long = -1
-        transaction.executeWithoutResult {
-            val root = rootRepo.persist(Root())
-            rootId = root.id
-            val branchA = branchRepo.persist(Branch())
-            branchAId = branchA.id
-            branchBId = branchRepo.persist(Branch()).id
-            branchABId = branchRepo.persist(Branch()).id
-
-            rootRepo.update(
-                root.copy(branchAKey = branchAId, branchBKey = branchBId)
-            )
-            // Reference to itself on "branchA"!
-            branchRepo.update(
-                branchA.copy(branchAKey = branchAId, branchBKey = branchABId)
-            )
-        }
-
-        transaction.executeWithoutResult {
-            val selection = RootDSL.select {
-                this.branchA {
-                    this.branchA {
-                        this.branchA { }
-                        this.branchB {  }
-                    }
-                    this.branchB {
-                        this.branchA {  }
-                        this.branchB {  }
-                    }
-                }
-                this.branchB {
-                    this.branchA {
-                        this.branchA { }
-                        this.branchB {  }
-                    }
-                    this.branchB {
-                        this.branchA {  }
-                        this.branchB {  }
-                    }
-                }
+            this.branchB {
+              this.branchA {}
+              this.branchB {}
             }
-
-            // TODO: Replace with selectById
-            val root = rootRepo.select(selection) {
-                where(RootDSL.id.eq(rootId))
-            }.firstOrNull()
-
-            assertNotNull(root)
-            assertEquals(rootId, root.id)
-            val branchA = root.branchA
-            assertNotNull(branchA)
-            assertEquals(branchAId, branchA.id)
-            val branchAA = branchA.branchA
-            assertNotNull(branchAA)
-            assertEquals(branchAId, branchAA.id)
-            val branchAAA = branchAA.branchA
-            assertNotNull(branchAAA)
-            assertEquals(branchAId, branchAAA.id)
-            assertNull(branchAAA.branchA)
-            assertNull(branchAAA.branchB)
-
-            val branchAB = branchA.branchB
-            assertNotNull(branchAB)
-            assertEquals(branchABId, branchAB.id)
-            assertNull(branchAB.branchA)
-            assertNull(branchAB.branchB)
-
-            // TODO: Investigate in the future: Not set branch keys are 0 instead of `null`
-            val branchB = root.branchB
-            assertNotNull(branchB)
-            assertEquals(branchBId, branchB.id)
-            assertNull(branchB.branchA)
-            assertNull(branchB.branchB)
+          }
+          this.branchB {
+            this.branchA {
+              this.branchA {}
+              this.branchB {}
+            }
+            this.branchB {
+              this.branchA {}
+              this.branchB {}
+            }
+          }
         }
+
+      // TODO: Replace with selectById
+      val root = rootRepo.select(selection) { where(RootDSL.id.eq(rootId)) }.firstOrNull()
+
+      assertNotNull(root)
+      assertEquals(rootId, root.id)
+      val branchA = root.branchA
+      assertNotNull(branchA)
+      assertEquals(branchAId, branchA.id)
+      val branchAA = branchA.branchA
+      assertNotNull(branchAA)
+      assertEquals(branchAAId, branchAA.id)
+      assertNull(branchAA.branchA)
+      assertNull(branchAA.branchB)
+      val branchAB = branchA.branchB
+      assertNotNull(branchAB)
+      assertEquals(branchABId, branchAB.id)
+      assertNull(branchAB.branchA)
+      assertNull(branchAB.branchB)
+
+      val branchB = root.branchB
+      assertNotNull(branchB)
+      assertEquals(branchBId, branchB.id)
+      assertNull(branchB.branchA)
+      assertNull(branchB.branchB)
     }
+  }
+
+  @Test
+  fun `select recursive selection`() {
+    var rootId: Long = -1
+    var branchAId: Long = -1
+    var branchBId: Long = -1
+    var branchABId: Long = -1
+    transaction.executeWithoutResult {
+      val root = rootRepo.persist(Root())
+      rootId = root.id
+      val branchA = branchRepo.persist(Branch())
+      branchAId = branchA.id
+      branchBId = branchRepo.persist(Branch()).id
+      branchABId = branchRepo.persist(Branch()).id
+
+      rootRepo.update(root.copy(branchAKey = branchAId, branchBKey = branchBId))
+      // Reference to itself on "branchA"!
+      branchRepo.update(branchA.copy(branchAKey = branchAId, branchBKey = branchABId))
+    }
+
+    transaction.executeWithoutResult {
+      val selection =
+        RootDSL.select {
+          this.branchA {
+            this.branchA {
+              this.branchA {}
+              this.branchB {}
+            }
+            this.branchB {
+              this.branchA {}
+              this.branchB {}
+            }
+          }
+          this.branchB {
+            this.branchA {
+              this.branchA {}
+              this.branchB {}
+            }
+            this.branchB {
+              this.branchA {}
+              this.branchB {}
+            }
+          }
+        }
+
+      // TODO: Replace with selectById
+      val root = rootRepo.select(selection) { where(RootDSL.id.eq(rootId)) }.firstOrNull()
+
+      assertNotNull(root)
+      assertEquals(rootId, root.id)
+      val branchA = root.branchA
+      assertNotNull(branchA)
+      assertEquals(branchAId, branchA.id)
+      val branchAA = branchA.branchA
+      assertNotNull(branchAA)
+      assertEquals(branchAId, branchAA.id)
+      val branchAAA = branchAA.branchA
+      assertNotNull(branchAAA)
+      assertEquals(branchAId, branchAAA.id)
+      assertNull(branchAAA.branchA)
+      assertNull(branchAAA.branchB)
+
+      val branchAB = branchA.branchB
+      assertNotNull(branchAB)
+      assertEquals(branchABId, branchAB.id)
+      assertNull(branchAB.branchA)
+      assertNull(branchAB.branchB)
+
+      // TODO: Investigate in the future: Not set branch keys are 0 instead of `null`
+      val branchB = root.branchB
+      assertNotNull(branchB)
+      assertEquals(branchBId, branchB.id)
+      assertNull(branchB.branchA)
+      assertNull(branchB.branchB)
+    }
+  }
 }
--- bee.persistent.test/src/test/kotlin/com/beeproduced/bee/persistent/test/select/SelectOrderByATest.kt
+++ bee.persistent.test/src/test/kotlin/com/beeproduced/bee/persistent/test/select/SelectOrderByATest.kt
@@ -4,6 +4,9 @@ import com.beeproduced.bee.persistent.test.config.ATestConfig
 import com.beeproduced.datasource.a.*
 import com.beeproduced.datasource.a.dsl.WeirdClassDSL
 import jakarta.persistence.EntityManager
+import java.util.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
@@ -15,13 +18,8 @@ import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.support.TransactionTemplate
-import java.util.*
-import kotlin.test.Test
-import kotlin.test.assertEquals
 
 /**
- *
- *
  * @author Kacper Urbaniec
  * @version 2024-01-15
  */
@@ -30,102 +28,89 @@ import kotlin.test.assertEquals
 @TestPropertySource("classpath:application.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SelectOrderByATest(
-    @Qualifier("aEM")
-    val em: EntityManager,
-    @Qualifier("aTM")
-    transactionManager: PlatformTransactionManager,
-    @Autowired
-    val weirdRepo: WeirdClassRepository
+  @Qualifier("aEM") val em: EntityManager,
+  @Qualifier("aTM") transactionManager: PlatformTransactionManager,
+  @Autowired val weirdRepo: WeirdClassRepository,
 ) {
-    private val transaction = TransactionTemplate(transactionManager)
+  private val transaction = TransactionTemplate(transactionManager)
 
-    @BeforeAll
-    fun beforeAll() = clear()
+  @BeforeAll fun beforeAll() = clear()
 
-    @AfterEach
-    fun afterEach() = clear()
+  @AfterEach fun afterEach() = clear()
 
-    fun clear() {
-        transaction.executeWithoutResult {
-            weirdRepo.cbf.delete(em, WeirdClass::class.java).executeUpdate()
-        }
+  fun clear() {
+    transaction.executeWithoutResult {
+      weirdRepo.cbf.delete(em, WeirdClass::class.java).executeUpdate()
     }
+  }
 
-    @Test
-    fun `order by asc`() {
-        transaction.executeWithoutResult {
-            val id = UUID.randomUUID()
-            val fooBar = FooBar("foo", "bar")
-            val foxtrot = Foxtrot("Foxtrot")
-            weirdRepo.persist(WeirdClass(id, fooBar, foxtrot))
-            val id2 = UUID.randomUUID()
-            val fooBar2 = FooBar("foo", "bar2")
-            val foxtrot2 = Foxtrot("Foxtrot2")
-            weirdRepo.persist(WeirdClass(id2, fooBar2, foxtrot2))
-            val id3 = UUID.randomUUID()
-            val fooBar3 = FooBar("foo", "bar3")
-            val foxtrot3 = Foxtrot("Foxtrot3")
-            weirdRepo.persist(WeirdClass(id3, fooBar3, foxtrot3))
+  @Test
+  fun `order by asc`() {
+    transaction.executeWithoutResult {
+      val id = UUID.randomUUID()
+      val fooBar = FooBar("foo", "bar")
+      val foxtrot = Foxtrot("Foxtrot")
+      weirdRepo.persist(WeirdClass(id, fooBar, foxtrot))
+      val id2 = UUID.randomUUID()
+      val fooBar2 = FooBar("foo", "bar2")
+      val foxtrot2 = Foxtrot("Foxtrot2")
+      weirdRepo.persist(WeirdClass(id2, fooBar2, foxtrot2))
+      val id3 = UUID.randomUUID()
+      val fooBar3 = FooBar("foo", "bar3")
+      val foxtrot3 = Foxtrot("Foxtrot3")
+      weirdRepo.persist(WeirdClass(id3, fooBar3, foxtrot3))
 
-            val ws = weirdRepo.select {
-                orderBy(WeirdClassDSL.foxtrot.asc())
-            }
-            assertEquals(id, ws[0].id)
-            assertEquals(id2, ws[1].id)
-            assertEquals(id3, ws[2].id)
-        }
+      val ws = weirdRepo.select { orderBy(WeirdClassDSL.foxtrot.asc()) }
+      assertEquals(id, ws[0].id)
+      assertEquals(id2, ws[1].id)
+      assertEquals(id3, ws[2].id)
     }
+  }
 
-    @Test
-    fun `order by desc`() {
-        transaction.executeWithoutResult {
-            val id = UUID.randomUUID()
-            val fooBar = FooBar("foo", "bar")
-            val foxtrot = Foxtrot("Foxtrot")
-            weirdRepo.persist(WeirdClass(id, fooBar, foxtrot))
-            val id2 = UUID.randomUUID()
-            val fooBar2 = FooBar("foo", "bar2")
-            val foxtrot2 = Foxtrot("Foxtrot2")
-            weirdRepo.persist(WeirdClass(id2, fooBar2, foxtrot2))
-            val id3 = UUID.randomUUID()
-            val fooBar3 = FooBar("foo", "bar3")
-            val foxtrot3 = Foxtrot("Foxtrot3")
-            weirdRepo.persist(WeirdClass(id3, fooBar3, foxtrot3))
+  @Test
+  fun `order by desc`() {
+    transaction.executeWithoutResult {
+      val id = UUID.randomUUID()
+      val fooBar = FooBar("foo", "bar")
+      val foxtrot = Foxtrot("Foxtrot")
+      weirdRepo.persist(WeirdClass(id, fooBar, foxtrot))
+      val id2 = UUID.randomUUID()
+      val fooBar2 = FooBar("foo", "bar2")
+      val foxtrot2 = Foxtrot("Foxtrot2")
+      weirdRepo.persist(WeirdClass(id2, fooBar2, foxtrot2))
+      val id3 = UUID.randomUUID()
+      val fooBar3 = FooBar("foo", "bar3")
+      val foxtrot3 = Foxtrot("Foxtrot3")
+      weirdRepo.persist(WeirdClass(id3, fooBar3, foxtrot3))
 
-            val ws = weirdRepo.select {
-                orderBy(WeirdClassDSL.foxtrot.desc())
-            }
-            assertEquals(id3, ws[0].id)
-            assertEquals(id2, ws[1].id)
-            assertEquals(id, ws[2].id)
-        }
+      val ws = weirdRepo.select { orderBy(WeirdClassDSL.foxtrot.desc()) }
+      assertEquals(id3, ws[0].id)
+      assertEquals(id2, ws[1].id)
+      assertEquals(id, ws[2].id)
     }
+  }
 
-    @Test
-    fun `order by multiple columns desc`() {
-        transaction.executeWithoutResult {
-            val id = UUID.randomUUID()
-            val fooBar = FooBar("foo", "bar")
-            val foxtrot = Foxtrot("Foxtrot")
-            weirdRepo.persist(WeirdClass(id, fooBar, foxtrot))
-            val id2 = UUID.randomUUID()
-            val fooBar2 = FooBar("foo", "bar2")
-            val foxtrot2 = Foxtrot("FoxtrotDuplicate")
-            weirdRepo.persist(WeirdClass(id2, fooBar2, foxtrot2))
-            val id3 = UUID.randomUUID()
-            val fooBar3 = FooBar("foo", "bar3")
-            val foxtrot3 = Foxtrot("FoxtrotDuplicate")
-            weirdRepo.persist(WeirdClass(id3, fooBar3, foxtrot3))
+  @Test
+  fun `order by multiple columns desc`() {
+    transaction.executeWithoutResult {
+      val id = UUID.randomUUID()
+      val fooBar = FooBar("foo", "bar")
+      val foxtrot = Foxtrot("Foxtrot")
+      weirdRepo.persist(WeirdClass(id, fooBar, foxtrot))
+      val id2 = UUID.randomUUID()
+      val fooBar2 = FooBar("foo", "bar2")
+      val foxtrot2 = Foxtrot("FoxtrotDuplicate")
+      weirdRepo.persist(WeirdClass(id2, fooBar2, foxtrot2))
+      val id3 = UUID.randomUUID()
+      val fooBar3 = FooBar("foo", "bar3")
+      val foxtrot3 = Foxtrot("FoxtrotDuplicate")
+      weirdRepo.persist(WeirdClass(id3, fooBar3, foxtrot3))
 
-            val ws = weirdRepo.select {
-                orderBy(
-                    WeirdClassDSL.foxtrot.desc(),
-                    WeirdClassDSL.fooBar.desc()
-                )
-            }
-            assertEquals(id3, ws[0].id)
-            assertEquals(id2, ws[1].id)
-            assertEquals(id, ws[2].id)
-        }
+      val ws =
+        weirdRepo.select { orderBy(WeirdClassDSL.foxtrot.desc(), WeirdClassDSL.fooBar.desc()) }
+      assertEquals(id3, ws[0].id)
+      assertEquals(id2, ws[1].id)
+      assertEquals(id, ws[2].id)
     }
+  }
 }
