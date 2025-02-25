da8675bc95e557fefe0ab31f91803f2fffe678d5
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2025-02-25 13:09:30 +0100

    Fixed excludes in bee.persistent.test for test configuration

--- bee.persistent.test/src/test/kotlin/com/beeproduced/bee/persistent/test/config/ATestConfig.kt
+++ bee.persistent.test/src/test/kotlin/com/beeproduced/bee/persistent/test/config/ATestConfig.kt
@@ -2,6 +2,7 @@ package com.beeproduced.bee.persistent.test.config
 
 import com.netflix.graphql.dgs.autoconfig.DgsExtendedScalarsAutoConfiguration
 import com.netflix.graphql.dgs.pagination.DgsPaginationAutoConfiguration
+import name.nkonev.multipart.springboot.graphql.server.MultipartGraphQlWebMvcAutoconfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.graphql.GraphQlAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -17,6 +18,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
       GraphQlAutoConfiguration::class,
       DgsPaginationAutoConfiguration::class,
       DgsExtendedScalarsAutoConfiguration::class,
+      MultipartGraphQlWebMvcAutoconfiguration::class,
     ],
   excludeName =
     ["com.netflix.graphql.dgs.springgraphql.autoconfig.DgsSpringGraphQLAutoConfiguration"],
