21b821a3fd95d7eb4bf57d78c0d55a5f1429d91c
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2025-06-16 13:12:25 +0200

    Fixed TransactionalResultAspect joinPoint as concrete result was mentioned

--- bee.functional/src/persistent/kotlin/com/beeproduced/bee/functional/persistent/transactional/TransactionalResultAspect.kt
+++ bee.functional/src/persistent/kotlin/com/beeproduced/bee/functional/persistent/transactional/TransactionalResultAspect.kt
@@ -1,10 +1,11 @@
 package com.beeproduced.bee.functional.persistent.transactional
 
+import com.beeproduced.bee.functional.extensions.com.github.michaelbull.result.failureAsErr
+import com.beeproduced.bee.functional.extensions.com.github.michaelbull.result.isFailure
 import com.beeproduced.bee.functional.result.errors.AppError
 import com.beeproduced.bee.functional.result.errors.InternalAppError
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Result
-import com.github.michaelbull.result.onFailure
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
@@ -24,8 +25,12 @@ import org.springframework.transaction.support.TransactionTemplate
 class TransactionalResultAspect(private val context: ApplicationContext) {
   private val logger = LoggerFactory.getLogger(TransactionalResultAspect::class.java)
 
+  // Previously was
+  // "execution(@com.beeproduced.bee.functional.persistent.transactional.TransactionalResult
+  //   com.github.michaelbull.result.Result *(..))"
+  // However, as Result is an inline value class now, it does not exist at runtime to point…
   @Pointcut(
-    "execution(@com.beeproduced.bee.functional.persistent.transactional.TransactionalResult com.github.michaelbull.result.Result *(..))"
+    "@annotation(com.beeproduced.bee.functional.persistent.transactional.TransactionalResult)"
   )
   fun transactionalMethodReturningResult() = Unit
 
@@ -53,15 +58,22 @@ class TransactionalResultAspect(private val context: ApplicationContext) {
     return template.execute { transaction ->
       val result =
         try {
-          joinPoint.proceed() as Result<*, AppError>
+          joinPoint.proceed()
+          // boxResult(value) as Result<*, AppError>
         } catch (ex: Throwable) {
           Err(InternalAppError(transactionalAnnotation.exceptionDescription, ex))
         }
 
-      result.onFailure {
-        logger.debug("Transaction failed, rolling back. Error: ${it.stackTraceToString()}")
+      if (isFailure(result)) {
+        val e = failureAsErr(result).error as? AppError
+        logger.debug("Transaction failed, rolling back. Error: ${e?.stackTraceToString()}")
         transaction.setRollbackOnly()
       }
+
+      // result.onFailure {
+      //   logger.debug("Transaction failed, rolling back. Error: ${it.stackTraceToString()}")
+      //   transaction.setRollbackOnly()
+      // }
       result
     }
   }
